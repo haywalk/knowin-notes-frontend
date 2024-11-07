cbb69cff27b1ac6a1d17f7912c652aadf48c08d3
Sawyer Stanley <sawyerscoding@gmail.com>
2024-11-07 16:57:38 -0400

    Testing rest.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { updateGameState } from './REST.js'
+import GameState from './GameState.js'
+
+
+const gs = new GameState();
+
+updateGameState(gs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
