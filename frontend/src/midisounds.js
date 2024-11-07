e6cb270a83404c5f3bc6fe95e45f7f8d1d8d2fc3
Sawyer Stanley <sawyerscoding@gmail.com>
2024-11-07 14:59:17 -0400

    Moved MIDI sounds to class

--- frontend/src/midisounds.js
+++ frontend/src/midisounds.js
@@ -1,12 +1,10 @@
 // Followed this resource VERY closely.
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_MIDI_API
 
-let midi = null; // global MIDIAccess object
-let NOTE_ON = 0b1001;
-let NOTE_OFF = 0b1000;
-let OTHER = 0;
 
-const audios = [];
+const NOTE_ON = 0b1001;
+const NOTE_OFF = 0b1000;
+const OTHER = 0;
 
 const soundfiles = [
     "bkgd.wav", "bkgd.wav", "bkgd.wav", "bkgd.wav", "bkgd.wav", "bkgd.wav",
@@ -15,93 +13,134 @@ const soundfiles = [
     "fs4.wav", "g4.wav", "gs4.wav", "a4.wav", "as4.wav", "b4.wav"
 ];
 
-export function initializeMIDI(){
-    // Create an oscillator for each possible MIDI key
-    for(let i = 0; i < 128; i++){
-        audios[i] = null; 
-    }
-    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-}
+export default class MIDIKeyboard{
 
-function onMIDISuccess(midiAccess) {
-    console.log("MIDI ready!");
-    midi = midiAccess; // store in the global (in real usage, would probably keep in an object instance)
-    listInputsAndOutputs(midi);
-    startLoggingMIDIInput(midi);
-}
+    #isLogging;
+    #midi;
+    #isEnabled;
+    #audios;
+    #noteOffset;
 
-function onMIDIFailure(msg) {
-    console.error(`Failed to get MIDI access - ${msg}`);
-}
-
-function listInputsAndOutputs(midiAccess) {
-    console.log(`Number of MIDI inputs: ${midiAccess.inputs.size}`);
-    console.log(`Number of MIDI outputs: ${midiAccess.outputs.size}`);
-    for (const entry of midiAccess.inputs) {
-        const input = entry[1];
-        console.log(
-            `Input port [type:'${input.type}']` +
-            ` id:'${input.id}'` +
-            ` manufacturer:'${input.manufacturer}'` +
-            ` name:'${input.name}'` +
-            ` version:'${input.version}'`,
-        );
+    constructor(){
+        this.#isLogging = false;
+        this.#midi = null;
+        this.isEnabled = false;
+        this.#audios = [];
+        this.#noteOffset = 49;
     }
 
-    for (const entry of midiAccess.outputs) {
-        const output = entry[1];
-        console.log(
-            `Output port [type:'${output.type}'] id:'${output.id}' manufacturer:'${output.manufacturer}' name:'${output.name}' version:'${output.version}'`,
-        );
-    }
-}
-  
-function startLoggingMIDIInput(midiAccess) {
-    midiAccess.inputs.forEach((entry) => {
-        entry.onmidimessage = onMIDIMessage;
-    });
-}
-
-function onMIDIMessage(event) {
-    // Status, note, velocity
-    // 0x90 0x32 0x00
-    var status = event.data[0];
-    var note = event.data[1];
-    var velocity = event.data[2];
-
-    var code = status >> 4;
-
-    if(code === NOTE_OFF || (code === NOTE_ON && velocity === 0)){
-        // stop playing note!
-        onNoteOff(note);
-    }
-    else if(code === NOTE_ON){
-        // play note!
-        onNoteOn(note);
-    }
-    else{
-        console.log("OTHER");
+    tryConnect(){
+        navigator.requestMIDIAccess().then(this.#onMIDISuccess, this.#onMIDIFailure);
     }
 
-    // Print full message
-    let str = "";
-    for (const character of event.data) {
-        str += `0x${character.toString(16)} `;
+    #onMIDISuccess(midiAccess){
+        // make space for audio instances (necessary?)
+        for(let i = 0; i < 128; i++){
+            audios[i] = null; 
+        }
+        this.#midi = midiAccess;
+        this.#isEnabled = true;
+        listInputsAndOutputs();
+        this.#startListeningForMIDIMessages();
+        console.log("MIDI ready!");
     }
-    console.log(str);
+
+    #onMIDIFailure(msg) {
+        console.error(`Failed to get MIDI access - ${msg}`);
+        this.#isEnabled = false;
+    }
+
+    listInputsAndOutputs() {
+        if(this.#midi === null){
+            console.log("No MIDI keyboard connected!");
+            return;
+        }
+        console.log(`Number of MIDI inputs: ${this.#midi.inputs.size}`);
+        console.log(`Number of MIDI outputs: ${this.#midi.outputs.size}`);
+        for (const entry of this.#midi.inputs) {
+            const input = entry[1];
+            console.log(
+                `Input port [type:'${input.type}']` +
+                ` id:'${input.id}'` +
+                ` manufacturer:'${input.manufacturer}'` +
+                ` name:'${input.name}'` +
+                ` version:'${input.version}'`,
+            );
+        }
+    
+        for (const entry of this.#midi.outputs) {
+            const output = entry[1];
+            console.log(
+                `Output port [type:'${output.type}'] id:'${output.id}' manufacturer:'${output.manufacturer}' name:'${output.name}' version:'${output.version}'`,
+            );
+        }
+    }
+      
+    #startListeningForMIDIMessages() {
+        midiAccess.inputs.forEach((entry) => {
+            entry.onmidimessage = onMIDIMessage;
+        });
+    }
+    
+    onMIDIMessage(event) {
+        // Status, note, velocity
+        // 0x90 0x32 0x00
+        var status = event.data[0];
+        var note = event.data[1];
+        var velocity = event.data[2];
+    
+        var code = status >> 4;
+    
+        if(code === NOTE_OFF || (code === NOTE_ON && velocity === 0)){
+            // stop playing note!
+            this.#onNoteOff(note);
+        }
+        else if(code === NOTE_ON){
+            // play note!
+            this.#onNoteOn(note);
+        }
+        else{
+            if(this.#isLogging) console.log("OTHER");
+        }
+    
+        if(this.#isLogging){
+            let str = "";
+            for (const character of event.data) {
+                str += `0x${character.toString(16)} `;
+            }
+            console.log(str);
+        }
+    }
+    
+    #onNoteOn(note){
+        if(this.#isLogging) console.log("NOTE_ON! (" + (note - this.#noteOffset) + ")");
+        this.#audios[note] = new Audio("src/assets/" + soundfiles[note - this.#noteOffset]);
+        this.#audios[note].play();
+    }
+    
+    #onNoteOff(note){
+        if(this.#isLogging) console.log("NOTE OFF!");
+        // Should maybe fade the sound of the note here somehow
+        audios[note].pause();
+        audios[note].currentTime = 0;
+    }
+
+    startLogging(){
+        this.#isLogging = true;
+    }
+
+    stopLogging(){
+        this.#isLogging = false;
+    }
+
+    get isEnabled(){
+        return this.#isEnabled;
+    }
+
+    get isLogging(){
+        return this.#isLogging;
+    }
 }
 
-function onNoteOn(note){
-    let offset = 49;
-    console.log("NOTE_ON! (" + (note - offset) + ")");
-    audios[note] = new Audio("src/assets/" + soundfiles[note - offset]);
-    audios[note].play();
-}
 
-function onNoteOff(note){
-    console.log("NOTE OFF!");
-    // Should maybe fade the sound of the note here somehow
-    audios[note].pause();
-    audios[note].currentTime = 0;
-}
   
