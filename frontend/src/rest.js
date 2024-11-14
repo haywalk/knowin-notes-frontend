09a800173688bd93896da6552dd35426a141cace
Sawyer Stanley <sawyerscoding@gmail.com>
2024-11-13 21:08:37 -0400

    API calls mostly working. Parsing must be dicussed

--- frontend/src/rest.js
+++ frontend/src/rest.js
@@ -1,21 +1,42 @@
 
-import GameState from "./GameState.js"
-
 export function updateGameState(gamestate){
-    const url = `http://localhost:8088/api/GET_STATE?old=${JSON.stringify(gamestate)}`;
-    console.log(url);
+
+    var b64 = btoa(JSON.stringify(gamestate));
+
+    const url = `http://localhost:8088/api/GET_STATE?old=${b64}`;
 
     const xhr = new XMLHttpRequest();
     xhr.open("GET", url);
     xhr.send();
 
-    xhr.onload = () => 
-    {
-        if (xhr.readyState == 4 && xhr.status == 200) 
-        {
-            return JSON.parse(xhr.response);
-        } else 
-        {
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200){
+            // Parse out the JSON string and return the game state object
+            // It may be a report object so check the returned object first
+            console.log(xhr.responseText);
+            console.log(atob(xhr.responseText));
+            // return JSON.parse(xhr.responseText);
+        } 
+        else{
+            console.log(`Error: ${xhr.status}`);
+        }
+    };
+}
+
+export function getHistory(){
+
+    const url = `http://localhost:8088/api/LIST_REPORTS`;
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200){
+            // Parse out the history JSON and return it.
+            console.log(xhr.responseText);
+            // return JSON.parse(xhr.responseText);
+        } 
+        else{
             console.log(`Error: ${xhr.status}`);
         }
     };
