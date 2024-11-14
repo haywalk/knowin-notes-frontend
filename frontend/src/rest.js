e3ad4f6cb30b5c9de59abd901c2bdb5f2acceb75
Sawyer Stanley <sawyerscoding@gmail.com>
2024-11-13 21:25:04 -0400

    Switched to usin gaxios for API calls. Much cleaner

--- frontend/src/rest.js
+++ frontend/src/rest.js
@@ -1,43 +1,31 @@
+import axios from 'axios';
 
-export function updateGameState(gamestate){
-
+export function updateGameState(gamestate) {
     var b64 = btoa(JSON.stringify(gamestate));
-
     const url = `http://localhost:8088/api/GET_STATE?old=${b64}`;
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.send();
-
-    xhr.onload = () => {
-        if (xhr.readyState == 4 && xhr.status == 200){
-            // Parse out the JSON string and return the game state object
-            // It may be a report object so check the returned object first
-            console.log(xhr.responseText);
-            console.log(atob(xhr.responseText));
-            // return JSON.parse(xhr.responseText);
-        } 
-        else{
-            console.log(`Error: ${xhr.status}`);
-        }
-    };
+    axios.get(url)
+        .then(response => {
+            // Parse out the game state if a gamestate is returned
+            // Parse out the report if a report is returned
+            console.log(response.data);
+            console.log(atob(response.data));
+        })
+        .catch(error => {
+            console.log(`Error: ${error.response.status}`);
+        });
 }
 
-export function getHistory(){
-
+export function getHistory() {
     const url = `http://localhost:8088/api/LIST_REPORTS`;
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.send();
 
-    xhr.onload = () => {
-        if (xhr.readyState == 4 && xhr.status == 200){
+    axios.get(url)
+        .then(response => {
             // Parse out the history JSON and return it.
-            console.log(xhr.responseText);
-            // return JSON.parse(xhr.responseText);
-        } 
-        else{
-            console.log(`Error: ${xhr.status}`);
-        }
-    };
+            console.log(response.data);
+            // return JSON.parse(response.data);
+        })
+        .catch(error => {
+            console.log(`Error: ${error.response.status}`);
+        });
 }
