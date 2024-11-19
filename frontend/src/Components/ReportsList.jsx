f2baa915c4623fae3a0fd66cfdef413aacb45d2a
anthony-arseneau <anthonyfarseneau@icloud.com>
2024-11-19 17:52:02 -0400

    Differentiate reports by game mode

--- frontend/src/Components/ReportsList.jsx
+++ frontend/src/Components/ReportsList.jsx
@@ -26,20 +26,17 @@ export default function ReportsList() {
     return (
         <>
             {reports.map((item) => (
-
                     <Link key={item.id} to={`/report/${item.id}`}>
-                    <div className='card'>
                     <Report key={item.id} 
-                                id={item.id}
-                                date={item.date} 
-                                time={item.time} 
-                                type={item.type} 
-                                accuracy={item.accuracy} 
-                                chronometer={item.chronometer} 
-                                numNotes={item.numNotes} 
-                                numMistakes={item.numMistakes}
+                            id={item.id}
+                            date={item.date} 
+                            time={item.time} 
+                            type={item.type} 
+                            accuracy={item.accuracy} 
+                            chronometer={item.chronometer} 
+                            numNotes={item.numNotes} 
+                            numMistakes={item.numMistakes}
                     />
-                    </div>
                     </Link>
             ))}
         </>
